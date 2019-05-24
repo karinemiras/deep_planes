c447a0224e629d9b49e7da58e1227fd580384443
karinemiras <karine.smiras@gmail.com>
2019-05-24 16:09:16 +0200

    number of channels

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ parser.add_argument('--logdir',
 
 parser.add_argument('--batch_size',
                     type=int,
-                    default=8)
+                    default=128)
 
 parser.add_argument('--is_cross_entropy',
                     action='store_true',
